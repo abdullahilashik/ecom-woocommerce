4c7c6eb14084841c6c8a2b8f15b42b7df5f54e23
abdullahilashik <abdullah.ashik.arefin@gmail.com>
2025-03-18 12:18:43 +0600

    home url

--- header.php
+++ header.php
@@ -38,9 +38,7 @@
                 <div class="row">
                     <div class="col-xs-12 col-sm-12 col-md-10 logo-holder">
                         <div class="logo">
-                            <a href="/"> 
-                            <?php the_custom_logo(); ?>    
-                        </a>
+                            <a href="<?php echo bloginfo('url') ?>"> <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="logo"> </a>
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-2 animate-dropdown top-cart-row">
